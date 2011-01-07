099bb61fe367c16f5e041260bce8f8dbe94209c8
Florian Schulze <florian.schulze@gmx.net>
2011-01-07 15:02:42 +0100

    Second version.

--- sqlalchemy_schemadisplay.py
+++ sqlalchemy_schemadisplay.py
@@ -20,7 +20,7 @@ def _mk_label(mapper, show_operations, show_attributes, show_datatypes, bordersi
     if show_operations:
         html += '<TR><TD ALIGN="LEFT">%s</TD></TR>' % '<BR ALIGN="LEFT"/>'.join(
             '%s(%s)' % (name,", ".join(default is _mk_label and ("%s") % arg or ("%s=%s" % (arg,repr(default))) for default,arg in 
-                zip((func.func_defaults and len(func.func_code.co_varnames)-1-(len(func.func_defaults) or 0))*[_mk_label]+list(func.func_defaults or []), func.func_code.co_varnames[1:])
+                zip((func.func_defaults and len(func.func_code.co_varnames)-1-(len(func.func_defaults) or 0) or func.func_code.co_argcount-1)*[_mk_label]+list(func.func_defaults or []), func.func_code.co_varnames[1:])
             ))
             for name,func in mapper.class_.__dict__.items() if isinstance(func, types.FunctionType) and func.__module__ == mapper.class_.__module__
         )
@@ -30,7 +30,7 @@ def _mk_label(mapper, show_operations, show_attributes, show_datatypes, bordersi
 
 def create_uml_graph(mappers, show_operations=True, show_attributes=True, show_multiplicity_one=False, show_datatypes=True, linewidth=1.0, font="Bitstream-Vera Sans"):
     graph = pydot.Dot(prog='neato',mode="major",overlap="0",sep=0.01,pack=True,dim=3)
-    relations = []
+    relations = set()
     for mapper in mappers:
         graph.add_node(pydot.Node(mapper.class_.__name__,
             shape="plaintext", label=_mk_label(mapper, show_operations, show_attributes, show_datatypes, linewidth),
@@ -39,72 +39,51 @@ def create_uml_graph(mappers, show_operations=True, show_attributes=True, show_m
         if mapper.inherits:
             graph.add_edge(pydot.Edge(mapper.inherits.class_.__name__,mapper.class_.__name__,
                 arrowhead='none',arrowtail='empty', style="setlinewidth(%s)" % linewidth, arrowsize=linewidth))
-        for from_property, loader in mapper.properties.items():
+        for loader in mapper.iterate_properties:
             if isinstance(loader, PropertyLoader) and loader.select_mapper in mappers:
-                lcollection = False
-                for join, loaders in relations:
-                    if ((isinstance(join,tuple) and (
-                        join[0].compare(loader.primaryjoin) and join[1].compare(loader.secondaryjoin)
-                                or
-                        join[1].compare(loader.primaryjoin) and join[0].compare(loader.secondaryjoin)
-                        ))
-                            or loader.primaryjoin.compare(join)):
-                        lcollection = loaders
-                if not lcollection:
-                    lcollection = []
-                    relations.append((not loader.secondaryjoin and loader.primaryjoin or (loader.primaryjoin,loader.secondaryjoin),lcollection))
-                lcollection.append((from_property,loader))
-    for join, loaders in relations:
-        if len(loaders) > 2:
-            raise Exception("Warning: too many loaders for join %s" % join)
+                if hasattr(loader, 'reverse_property'):
+                    relations.add(frozenset([loader, loader.reverse_property]))
+                else:
+                    relations.add(frozenset([loader]))
+    for relation in relations:
+        #if len(loaders) > 2:
+        #    raise Exception("Warning: too many loaders for join %s" % join)
         args = {}
-        if len(loaders) == 1:
-            from_ = loaders[0][1].parent.class_
-            to = loaders[0][1].argument
-            args['headlabel'] = "+%s" % loaders[0][0]
-            if loaders[0][1].direction in (sync.ONETOMANY, sync.MANYTOMANY):
-                args['headlabel'] += " *"
-            elif any(col.nullable for col in loaders[0][1].remote_side):
-                args['headlabel'] += " 0..1"
-            elif show_multiplicity_one:
-                args['headlabel'] += " 1"
-            if loaders[0][1].direction in (sync.MANYTOONE, sync.MANYTOMANY):
-                args['taillabel'] = "*"
-            elif show_multiplicity_one:
-                args['taillabel'] = " 1"
-            args['arrowtail'] = 'none'
-            args['arrowhead'] = 'vee'
-        else:
-            if loaders[0][1].parent.class_ != loaders[1][1].argument or loaders[1][1].parent.class_ != loaders[0][1].argument:
-                raise Exception("A merged join %s has more than two associated classes %s" % (join,
-                    ", ".join(cls.__name__ for cls in set([
-                        loaders[0][1].parent.class_, loaders[1][1].argument,
-                        loaders[1][1].parent.class_, loaders[0][1].argument
-                    ]))
-                ))
-            from_ = loaders[0][1].argument
-            to = loaders[1][1].argument
-            args['headlabel'] = "+%s" % loaders[1][0]
-            args['taillabel'] = "+%s" % loaders[0][0]
-            if loaders[1][1].uselist:
-                args['headlabel'] += " *"
-            elif any(col.nullable for col in loaders[0][1].remote_side):
-                args['headlabel'] += " 0..1"
-            elif show_multiplicity_one:
-                args['headlabel'] += " 1"
-            if loaders[0][1].uselist:
-                args['taillabel'] += " *"
-            elif any(col.nullable for col in loaders[1][1].remote_side):
-                args['taillabel'] += " 0..1"
-            elif show_multiplicity_one:
-                args['taillabel'] += " 1"
-            args['constraint'] = False
+        def multiplicity_indicator(prop):
+            if prop.uselist:
+                return ' *'
+            if any(col.nullable for col in prop.local_side):
+                return ' 0..1'
+            if show_multiplicity_one:
+                return ' 1'
+            return ''
+        
+        if len(relation) == 2:
+            src, dest = relation
+            from_name = src.parent.class_.__name__
+            to_name = dest.parent.class_.__name__
+            
+            def calc_label(src,dest):
+                return '+' + src.key + multiplicity_indicator(src)
+            args['headlabel'] = calc_label(src,dest)
+            
+            args['taillabel'] = calc_label(dest,src)
             args['arrowtail'] = 'none'
             args['arrowhead'] = 'none'
-        graph.add_edge(pydot.Edge(from_.__name__,to.__name__,
+            args['constraint'] = False
+        else:
+            prop, = relation
+            from_name = prop.parent.class_.__name__
+            to_name = prop.mapper.class_.__name__
+            args['headlabel'] = '+%s%s' % (prop.key, multiplicity_indicator(prop))
+            args['arrowtail'] = 'none'
+            args['arrowhead'] = 'vee'
+            
+        graph.add_edge(pydot.Edge(from_name,to_name,
             fontname=font, fontsize=7.0, style="setlinewidth(%s)"%linewidth, arrowsize=linewidth,
             **args)
         )
+
     return graph
 
 from sqlalchemy.databases.postgres import PGDialect
@@ -124,13 +103,13 @@ def _render_table_html(table, metadata, show_indexes, show_datatypes):
     html = '<<TABLE BORDER="1" CELLBORDER="0" CELLSPACING="0"><TR><TD ALIGN="CENTER">%s</TD></TR><TR><TD BORDER="1" CELLPADDING="0"></TD></TR>' % table.name 
 
     html += ''.join('<TR><TD ALIGN="LEFT" PORT="%s">%s</TD></TR>' % (col.name, format_col_str(col)) for col in table.columns)
-    if isinstance(metadata.engine.dialect, PGDialect):
+    if isinstance(metadata.bind.dialect, PGDialect):
         # postgres engine doesn't reflect indexes
-        indexes = dict((name,defin) for name,defin in metadata.engine.execute(text("SELECT indexname, indexdef FROM pg_indexes WHERE tablename = '%s'" % table.name)))
+        indexes = dict((name,defin) for name,defin in metadata.bind.execute(text("SELECT indexname, indexdef FROM pg_indexes WHERE tablename = '%s'" % table.name)))
         if indexes and show_indexes:
             html += '<TR><TD BORDER="1" CELLPADDING="0"></TD></TR>'
             for index, defin in indexes.items():
-                ilabel = 'UNIQUE' in defin and 'UNIQUE' or 'INDEX'
+                ilabel = 'UNIQUE' in defin and 'UNIQUE ' or 'INDEX '
                 ilabel += defin[defin.index('('):]
                 html += '<TR><TD ALIGN="LEFT">%s</TD></TR>' % ilabel
     html += '</TABLE>>'
@@ -146,10 +125,9 @@ def create_schema_graph(tables=None, metadata=None, show_indexes=True, show_data
     if not metadata and len(tables):
         metadata = tables[0].metadata
     elif not tables and metadata:
-        if isinstance(metadata.engine.dialect, PGDialect):
-            tables = [Table(name, metadata, autoload=True) for (name,) in metadata.engine.execute(text("SELECT tablename FROM pg_tables WHERE schemaname = 'public'"))]
-        else:
-            raise Exception("Autloading tables currently works only on postgres")
+        if not len(metadata.tables):
+            metadata.reflect()
+        tables = metadata.tables.values()
     else:
         raise Exception("You need to specify at least tables or metadata")
     
