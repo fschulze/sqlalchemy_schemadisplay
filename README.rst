4195990cd80aaa916c8c50962303e026a7c33004
Florian Schulze <florian.schulze@gmx.net>
2011-01-07 16:09:51 +0100

    Make this an egg.

---
+++ README.rst
@@ -0,0 +1,6 @@
+sqlalchemy_schemadisplay
+========================
+
+Turn SQLAlchemy DB Model into a graph.
+
+See http://www.sqlalchemy.org/trac/wiki/UsageRecipes/SchemaDisplay
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+import os
+
+version = '1.0'
+
+long_description = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setup(
+    name='sqlalchemy_schemadisplay',
+    version=version,
+    description="Turn SQLAlchemy DB Model into a graph",
+    long_description=long_description[long_description.find('\n\n'):],
+    url='https://github.com/fschulze/sqlalchemy_schemadisplay',
+    py_modules=['sqlalchemy_schemadisplay'],
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        'pydot',
+    ],
+)
