ed6b4641971e45cac4c96ae76565d683d89dfca7
Anatoli Babenia <anatoli@rainforce.org>
2019-09-17 07:50:13 +0300

    Add primitive CLI interface

--- sqlalchemy_schemadisplay.py
+++ sqlalchemy_schemadisplay.py
@@ -225,3 +225,37 @@ def show_schema_graph(*args, **kwargs):
     from PIL import Image
     iostream = StringIO(create_schema_graph(*args, **kwargs).create_png())
     Image.open(iostream).show(command=kwargs.get('command','gwenview'))
+
+def main(dburli, outpng='dbschema.png'):
+    from sqlalchemy import MetaData
+    from sqlalchemy_schemadisplay import create_schema_graph
+
+    # create the pydot graph object by autoloading all tables via a bound metadata object
+    graph = create_schema_graph(metadata=MetaData(dburl),
+       show_datatypes=False, # The image would get nasty big if we'd show the datatypes
+       show_indexes=False, # ditto for indexes
+       rankdir='LR', # From left to right (instead of top to bottom)
+       concentrate=False # Don't try to join the relation lines together
+    )
+    print('writing {}'.format(outpng))
+    graph.write_png('dbschema.png') # write out the file
+
+
+if __name__ == '__main__':
+  usage = '''\
+usage: {py} <dburl>
+
+examples:
+
+  {py} postgres://user:pwd@host/database
+  {py} sqlite:////usr/share/my.db
+
+  '''.format(py=__file__)
+
+  import sys
+  if not sys.argv[1:]:
+    print(usage)
+    sys.exit()
+
+  dburl = sys.argv[1]
+  sys.exit(main(dburl))
